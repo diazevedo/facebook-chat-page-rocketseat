2c96fad61cd127c754e0829b2b7c4e3bec06eb23
Diego Azevedo <dapptn@hotmail.com>
2019-09-08 23:20:38 +1000

    save stage

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import profile from "../../assests/profile.png";
+
+const Header = () => {
+  <header>
+    <img src={profile}></img>
+  </header>;
+};
+
+export default Header;
